ca63fb134e55dc8ef3e1cd292fde061a48f94b2d
Ishaan Kapri <ishaankapri11@gmail.com>
2014-05-03 11:19:09 +0530

    minor change in txt template

---
+++ builder.py
@@ -0,0 +1,10 @@
+__author__ = 'ishaan'
+
+
+class Resume(object):
+    def __init__(self, name, personal, experience, education, skills):
+        self.name = name
+        self.personal = personal
+        self.experience = experience
+        self.education = education
+        self.skills = skills
--- templates.py
+++ templates.py
@@ -1,10 +1,11 @@
 __author__ = 'ishaan'
 
-TXT_TEMPLATE = '''\t\t\t\t{{name}}\n{% if dob or address or email %}\t\t\t\tPersonal\n{%endif%}{% if dob %}DOB : \
-{{dob}}\n{% endif %}{% if address %}ADDRESS : {{address}}\n{% endif %}{% if email %}EMAIL : {{email}}\
-\n{% endif %}{% if education %}\t\t\t\tEDUCATION\n{% for e in education %}{{e.school}}\n{{e.degree}}\n{{e.start_date}}-\
-{{e.end_date}}\n{% if e.description %}{{e.description}}\n{% endif %}\n{%endfor%}{% endif %}{% if experience %}\t\t\t\t\
-EXPERIENCE\n{% for e in experience %}{{e.company}}\n{{e.title}}\n{{e.start_date}}-{{e.end_date}}\n{%if e.description%}\
-{{e.description}}\n{% endif %}\n{%endfor%}{% endif %}{% if skills %}\t\t\t\t\SKILLS\n{% for s in skills%}{{s.name}}\n\
-{% endif %}\n{%endfor%}{% endif %}
+TXT_TEMPLATE = '''\t\t\t\t{{resume.name}}\n{%if resume.personal.dob or resume.personal.address or resume.personal.email%}\
+\t\t\t\tPersonal\n{%endif%}{% if resume.personal.dob %}DOB : {{resume.personal.dob}}\n{% endif %}\
+{% if resume.personal.address %}ADDRESS : {{resume.personal.address}}\n{% endif %}{% if resume.personal.email %}EMAIL :\
+ {{resume.personal.email}}\n{% endif %}{% if resume.education %}\t\t\t\tEDUCATION\n{% for e in resume.education %}\
+ {{e.school}}\n{{e.degree}}\n{{e.start_date}}-{{e.end_date}}\n{% if e.description %}{{e.description}}\n{% endif %}\n\
+ {%endfor%}{% endif %}{% if resume.experience %}\t\t\t\tEXPERIENCE\n{% for e in resume.experience %}{{e.company}}\n\
+ {{e.title}}\n{{e.start_date}}-{{e.end_date}}\n{%if e.description%}{{e.description}}\n{% endif %}\n{%endfor%}\
+ {% endif %}{% if resume.skills %}\t\t\t\tSKILLS\n{% for s in resume.skills%}{{s.name}}\n{%endfor%}{% endif %}
 '''
