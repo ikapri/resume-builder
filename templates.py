36b97b6cf3ee6f1c1e36fade27cf79314ebdb793
Ishaan Kapri <ishaankapri11@gmail.com>
2014-05-03 10:57:06 +0530

    .txt template done

--- templates.py
+++ templates.py
@@ -1,8 +1,10 @@
 __author__ = 'ishaan'
 
-TXT_TEMPLATE = '''\t\t\t\t{{name}}\n{% if dob %}DOB : {{dob}}\n{% endif %}{% if address %}ADDRESS : {{address}}\n{% endif %}{% if email %}EMAIL : {{email}}\
+TXT_TEMPLATE = '''\t\t\t\t{{name}}\n{% if dob or address or email %}\t\t\t\tPersonal\n{%endif%}{% if dob %}DOB : \
+{{dob}}\n{% endif %}{% if address %}ADDRESS : {{address}}\n{% endif %}{% if email %}EMAIL : {{email}}\
 \n{% endif %}{% if education %}\t\t\t\tEDUCATION\n{% for e in education %}{{e.school}}\n{{e.degree}}\n{{e.start_date}}-\
-{{e.end_date}}\n{% if e.description %}{{e.description}}\n{% endif %}\n{%endfor%}{% endif %}{% if experience %}EXPERIENCE\
-{% for e in experience %}{{e.company}}\n{{e.title}}\n{{e.start_date}}-{{e.end_date}}\n{% if e.description %}\
-{{e.description}}\n{% endif %}\n{%endfor%}{% endif %}
+{{e.end_date}}\n{% if e.description %}{{e.description}}\n{% endif %}\n{%endfor%}{% endif %}{% if experience %}\t\t\t\t\
+EXPERIENCE\n{% for e in experience %}{{e.company}}\n{{e.title}}\n{{e.start_date}}-{{e.end_date}}\n{%if e.description%}\
+{{e.description}}\n{% endif %}\n{%endfor%}{% endif %}{% if skills %}\t\t\t\t\SKILLS\n{% for s in skills%}{{s.name}}\n\
+{% endif %}\n{%endfor%}{% endif %}
 '''
