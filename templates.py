8f5368041a9ecd5fc88aab21595819d6149d6026
Ishaan Kapri <ishaankapri11@gmail.com>
2014-05-04 21:52:16 +0530

    change in TXT template and CSV template added

--- templates.py
+++ templates.py
@@ -1,11 +1,18 @@
 __author__ = 'ishaan'
 
-TXT_TEMPLATE = '''\t\t\t\t{{resume.name}}\n{%if resume.personal.dob or resume.personal.address or resume.personal.email%}\
-\t\t\t\tPersonal\n{%endif%}{% if resume.personal.dob %}DOB : {{resume.personal.dob}}\n{% endif %}\
-{% if resume.personal.address %}ADDRESS : {{resume.personal.address}}\n{% endif %}{% if resume.personal.email %}EMAIL :\
- {{resume.personal.email}}\n{% endif %}{% if resume.education %}\t\t\t\tEDUCATION\n{% for e in resume.education %}\
- {{e.school}}\n{{e.degree}}\n{{e.start_date}}-{{e.end_date}}\n{% if e.description %}{{e.description}}\n{% endif %}\n\
- {%endfor%}{% endif %}{% if resume.experience %}\t\t\t\tEXPERIENCE\n{% for e in resume.experience %}{{e.company}}\n\
- {{e.title}}\n{{e.start_date}}-{{e.end_date}}\n{%if e.description%}{{e.description}}\n{% endif %}\n{%endfor%}\
- {% endif %}{% if resume.skills %}\t\t\t\tSKILLS\n{% for s in resume.skills%}{{s.name}}\n{%endfor%}{% endif %}
+TXT_TEMPLATE = '''\t\t\t\t{{resume.name}}\n{% if resume.dob %}DOB : {{resume.dob}}\n{% endif %}\
+{% if resume.address %}ADDRESS : {{resume.address}}\n{% endif %}{% if resume.email %}EMAIL :\
+{{resume.email}}\n{% endif %}{% if resume.education %}\t\t\t\tEDUCATION\n{% for e in resume.education %}\
+{{e.school}}\n{{e.degree}}\n{{e.start_date}}-{{e.end_date}}\n{% if e.description %}{{e.description}}\n{% endif %}\n\
+{%endfor%}{% endif %}{% if resume.experience %}\t\t\t\tEXPERIENCE\n{% for e in resume.experience %}{{e.company}}\n\
+{{e.title}}\n{{e.start_date}}-{{e.end_date}}\n{%if e.description%}{{e.description}}\n{% endif %}\n{%endfor%}\
+{% endif %}{% if resume.skills %}\t\t\t\tSKILLS\n{{resume.skills}}\n{% endif %}
 '''
+
+CSV_TEMPLATE = '''NAME,{{resume.name}}\n{% if resume.dob %}DOB,{{resume.dob}}\n{% endif %}\
+{% if resume.address %}ADDRESS,{{resume.address}}\n{% endif %}{% if resume.email %}EMAIL,\
+{{resume.email}}\n{% endif %}{% if resume.education %}EDUCATION{% for e in resume.education %},\
+"{{e.school}}\n{{e.degree}}\n{{e.start_date}}-{{e.end_date}}\n{% if e.description %}{{e.description}}{% endif %}"\n\
+{%endfor%}{% endif %}{% if resume.experience %}EXPERIENCE{% for e in resume.experience %},"{{e.company}}\n\
+{{e.title}}\n{{e.start_date}}-{{e.end_date}}\n{%if e.description%}{{e.description}}{% endif %}"\n{%endfor%}\
+{% endif %}{% if resume.skills %}SKILLS,"{{resume.skills}}"{% endif %}'''
