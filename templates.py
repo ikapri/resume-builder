eef263c17fc67105bf35dad34792b0388bdbf293
Ishaan Kapri <ishaankapri11@gmail.com>
2014-05-04 22:15:57 +0530

    templates modifies

--- templates.py
+++ templates.py
@@ -2,17 +2,19 @@ __author__ = 'ishaan'
 
 TXT_TEMPLATE = '''\t\t\t\t{{resume.name}}\n{% if resume.dob %}DOB : {{resume.dob}}\n{% endif %}\
 {% if resume.address %}ADDRESS : {{resume.address}}\n{% endif %}{% if resume.email %}EMAIL :\
-{{resume.email}}\n{% endif %}{% if resume.education %}\t\t\t\tEDUCATION\n{% for e in resume.education %}\
-{{e.school}}\n{{e.degree}}\n{{e.start_date}}-{{e.end_date}}\n{% if e.description %}{{e.description}}\n{% endif %}\n\
-{%endfor%}{% endif %}{% if resume.experience %}\t\t\t\tEXPERIENCE\n{% for e in resume.experience %}{{e.company}}\n\
-{{e.title}}\n{{e.start_date}}-{{e.end_date}}\n{%if e.description%}{{e.description}}\n{% endif %}\n{%endfor%}\
-{% endif %}{% if resume.skills %}\t\t\t\tSKILLS\n{{resume.skills}}\n{% endif %}
+{{resume.email}}\n{% endif %}{% if resume.education %}\t\t\t\tEDUCATION{% for e in resume.education %}\n\
+SCHOOL : {{e.school}}\nDEGREE : {{e.degree}}\nTIME : {{e.start_date}}-{{e.end_date}}\n{% if e.description %}DESCRIPTION\
+ : {{e.description}}\n{% endif %}\n{%endfor%}{% endif %}{% if resume.experience %}\t\t\t\tEXPERIENCE\
+ {% for e in resume.experience %}\nCOMPANY : {{e.company}}\nTITLE : {{e.title}}\nTIME : {{e.start_date}}-{{e.end_date}}\
+ \n{%if e.description%}DESCRIPTION : {{e.description}}\n{% endif %}\n{%endfor%}{% endif %}{% if resume.skills %}\t\t\t\
+ \tSKILLS\n{{resume.skills}}\n{% endif %}
 '''
 
-CSV_TEMPLATE = '''NAME,{{resume.name}}\n{% if resume.dob %}DOB,{{resume.dob}}\n{% endif %}\
-{% if resume.address %}ADDRESS,{{resume.address}}\n{% endif %}{% if resume.email %}EMAIL,\
-{{resume.email}}\n{% endif %}{% if resume.education %}EDUCATION{% for e in resume.education %},\
-"{{e.school}}\n{{e.degree}}\n{{e.start_date}}-{{e.end_date}}\n{% if e.description %}{{e.description}}{% endif %}"\n\
-{%endfor%}{% endif %}{% if resume.experience %}EXPERIENCE{% for e in resume.experience %},"{{e.company}}\n\
-{{e.title}}\n{{e.start_date}}-{{e.end_date}}\n{%if e.description%}{{e.description}}{% endif %}"\n{%endfor%}\
-{% endif %}{% if resume.skills %}SKILLS,"{{resume.skills}}"{% endif %}'''
+CSV_TEMPLATE = '''NAME,"{{resume.name}}"\n{% if resume.dob %}DOB,"{{resume.dob}}"\n{% endif %}\
+{% if resume.address %}ADDRESS,"{{resume.address}}"\n{% endif %}{% if resume.email %}EMAIL,\
+"{{resume.email}}"\n{% endif %}{% if resume.education %}EDUCATION{% for e in resume.education %},\
+"SCHOOL : {{e.school}}\nDEGREE : {{e.degree}}\nTIME : {{e.start_date}}-{{e.end_date}}\n{% if e.description %}\
+DESCRIPTION : {{e.description}}{% endif %}"\n{%endfor%}{% endif %}{% if resume.experience %}EXPERIENCE\
+{% for e in resume.experience %},"COMPANY : {{e.company}}\nTITLE : {{e.title}}\nTIME : {{e.start_date}}-{{e.end_date}}\
+\n{%if e.description%}DESCRIPTION : {{e.description}}{% endif %}"\n{%endfor%}{% endif %}{% if resume.skills %}SKILLS,\
+"{{resume.skills}}"{% endif %}'''
